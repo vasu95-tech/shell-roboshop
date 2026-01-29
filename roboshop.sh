5647f92a7b0f3cc81b11704680382a6d220c9aab
vasu <vasumattipelli77@gmail.com>
2026-01-29 22:18:22 +0530

    05 program

--- roboshop.sh
+++ roboshop.sh
@@ -3,10 +3,11 @@
 Ami_id=ami-0220d79f3f480ecf5
 Security_group_id=sg-0ec1754eb45347fa5
 
-Instance_id=$(aws ec2 run-instances --image-id $Ami_id --instance-type t3.micro --security-group-ids $Security_group_id --tag-specifications
-'ResourceType=instance,Tags=[{Key=Name,Value=test}]' --query 'Instances[0].InstanceId' --output)
 for instance in $@
 do
+ Instance_id=$(aws ec2 run-instances --image-id $Ami_id --instance-type t3.micro --security-group-ids $Security_group_id --tag-specifications
+'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output)
+
  if [ $Instance != "frontend" ]; then
     ip=$(aws ec2 describe-instances --instance-ids $Instance_id --query 
     'Reservations[0].Instances[0].PrivateIpAddress' --output text)
