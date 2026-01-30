0a57679140fb94953ace5b78393cbc504749cc5c
vasu <vasumattipelli77@gmail.com>
2026-01-30 14:12:32 +0530

    roboshop

--- 1001.sh
+++ 1001.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
 
-Ami_id=ami-0220d79f3f480ecf5
-Security_group_id=sg-0ec1754eb45347fa5
-
-for instance in $@
-do
-
-  Instance_id=$(aws ec2 run-instances --image-id $Ami_id --instance-type t3.micro --security-group-ids $Security_group_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
- if [ $instance != "frontend" ]; then
-    ip=$(aws ec2 describe-instances --instance-ids $Instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
- else  
-    ip=$(aws ec2 describe-instances --instance-ids $Instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
- fi
-  echo $instance: $ip
-
-done
+echo "hello"
