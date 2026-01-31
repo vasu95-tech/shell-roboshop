6405411088165dd8b952826c596f6b2e0fcd2c59
vasu <vasumattipelli77@gmail.com>
2026-01-31 13:30:18 +0530

    roboshop

--- mongodb_config.sh
+++ mongodb_config.sh
@@ -6,9 +6,10 @@ Y="\e[33m"
 N="\e[0m"
 
 log_folder="/var/log/shell-roboshop"
-mkdir -p $log_folder
 script_name=$( echo $0 | cut -d "." f1 )
 log_file="$log_folder/$script_name.log"
+
+mkdir -p $log_folder
 
 if [ $userid -ne 0 ]; then
   echo "Run the script with root privelege"
